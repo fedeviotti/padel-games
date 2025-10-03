58f113566dd3d3195072affc61576152ccac9f7a
Federico Viotti <federico.viotti@archivagroup.it>
2025-10-04 00:06:20 +0200

    feat: add Gauge components to dashboard for displaying game statistics and update welcome message with user details

--- src/app/(tabs-layout)/page.tsx
+++ src/app/(tabs-layout)/page.tsx
@@ -1,9 +1,12 @@
-import { Box, Paper, Typography } from '@mui/material';
+'use client';
+
+import { Box, Divider, Paper, Typography } from '@mui/material';
+import { Gauge } from '@mui/x-charts/Gauge';
 import { useProtectedRoute } from '@/hooks/useProtectedRoute';
 import Loading from '../loading';
 
 export default function Dashboard() {
-  const { isChecking } = useProtectedRoute();
+  const { user, isChecking } = useProtectedRoute();
 
   if (isChecking) return <Loading />;
 
@@ -14,9 +17,30 @@ export default function Dashboard() {
           Dashboard
         </Typography>
         <Typography variant="body1">
-          Welcome to Padel Games! Here you can see an overview of your games, tournaments, and
-          player statistics.
+          Welcome to Padel Games {user?.displayName || user?.primaryEmail?.split('@')[0]}! Here you
+          can see statistics of your games.
         </Typography>
+        <Divider sx={{ my: 2 }} />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            backgroundColor: 'red',
+            gap: 2,
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flex: 1 }}>
+            <Typography variant="body1">Percentage of wins in games played last month</Typography>
+            <Gauge value={60} />
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flex: 1 }}>
+            <Typography variant="body1">
+              Percentage of wins in games played last month against selected player
+            </Typography>
+            <Gauge value={60} />
+          </Box>
+        </Box>
       </Paper>
     </Box>
   );
