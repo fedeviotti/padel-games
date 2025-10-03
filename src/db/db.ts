3680c9b24710da2709a7482824fe29cc66693e74
Federico Viotti <fedeviotti@gmail.com>
2025-10-03 15:45:08 +0200

    chore: format

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,8 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+import { neon } from '@neondatabase/serverless';
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-config({ path: ".env.local" }); // or .env.local
+config({ path: '.env.local' }); // or .env.local
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
