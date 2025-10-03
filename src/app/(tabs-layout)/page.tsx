9e6039d9826031b60c6b6f1f7301443e95f22c15
Federico Viotti <fedeviotti@gmail.com>
2025-10-04 00:35:25 +0200

    chore: add todo [skip ci]

--- src/app/(tabs-layout)/page.tsx
+++ src/app/(tabs-layout)/page.tsx
@@ -41,6 +41,7 @@ export default function Dashboard() {
           can see statistics of your games.
         </Typography>
         <Divider sx={{ my: 2 }} />
+        {/*TODO: Add charts and user selection here*/}
         <Box
           sx={{
             display: 'flex',
